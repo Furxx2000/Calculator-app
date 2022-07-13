072b2ff065bac8605b984c40aab13816fcfe5da6
furxx2000 <furxx2000@gmail.com>
2022-07-13 21:40:56 +0800

    Add new config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import Components from "unplugin-vue-components/vite";
+import postcssPresetEnv from "postcss-preset-env";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue(), Components({ dts: true })],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/css/style.scss";`,
+      },
+    },
+    postcss: {
+      plugins: [postcssPresetEnv],
+    },
+  },
+});
