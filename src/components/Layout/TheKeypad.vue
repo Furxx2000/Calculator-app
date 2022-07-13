45b193030f499a0cdf2dad7eb55030f01e449283
furxx2000 <furxx2000@gmail.com>
2022-07-13 23:25:04 +0800

    將div改成button, 增加aria-selected屬性, 點擊時動畫

--- src/components/Layout/TheKeypad.vue
+++ src/components/Layout/TheKeypad.vue
@@ -6,30 +6,76 @@ const emit = defineEmits(["get-key"]);
 function keyClick(e: MouseEvent) {
   const buttonEl = e.currentTarget as HTMLDivElement;
   const action = buttonEl.textContent;
+  const btnContainer = buttonEl.parentElement;
+  if (action === "+" || action === "-" || action === "x" || action === "/") {
+    btnContainer
+      ?.querySelector("[aria-selected='true']")
+      ?.setAttribute("aria-selected", "false");
+    buttonEl.setAttribute("aria-selected", "true");
+  } else {
+    btnContainer
+      ?.querySelector("[aria-selected='true']")
+      ?.setAttribute("aria-selected", "false");
+  }
+
   emit("get-key", action);
 }
 </script>
 
 <template>
   <section>
-    <div class="seven" @click="keyClick"><p>7</p></div>
-    <div class="eight" @click="keyClick"><p>8</p></div>
-    <div class="nine" @click="keyClick"><p>9</p></div>
-    <div class="delete uppercase" @click="keyClick"><p>del</p></div>
-    <div class="four" @click="keyClick"><p>4</p></div>
-    <div class="five" @click="keyClick"><p>5</p></div>
-    <div class="six" @click="keyClick"><p>6</p></div>
-    <div class="plus" @click="keyClick"><p>+</p></div>
-    <div class="one" @click="keyClick"><p>1</p></div>
-    <div class="two" @click="keyClick"><p>2</p></div>
-    <div class="three" @click="keyClick"><p>3</p></div>
-    <div class="minus" @click="keyClick"><p>-</p></div>
-    <div class="dot" @click="keyClick"><p>.</p></div>
-    <div class="zero" @click="keyClick"><p>0</p></div>
-    <div class="slash" @click="keyClick"><p>/</p></div>
-    <div class="multiply" @click="keyClick"><p>x</p></div>
-    <div class="reset uppercase" @click="keyClick"><p>reset</p></div>
-    <div class="equal" @click="keyClick"><p>=</p></div>
+    <button class="seven" aria-selected="false" @click="keyClick">
+      <p>7</p>
+    </button>
+    <button class="eight" aria-selected="false" @click="keyClick">
+      <p>8</p>
+    </button>
+    <button class="nine" aria-selected="false" @click="keyClick">
+      <p>9</p>
+    </button>
+    <button class="delete uppercase" @click="keyClick"><p>del</p></button>
+    <button class="four" aria-selected="false" @click="keyClick">
+      <p>4</p>
+    </button>
+    <button class="five" aria-selected="false" @click="keyClick">
+      <p>5</p>
+    </button>
+    <button class="six" aria-selected="false" @click="keyClick">
+      <p>6</p>
+    </button>
+    <button class="plus" aria-selected="false" @click="keyClick">
+      <p>+</p>
+    </button>
+    <button class="one" aria-selected="false" @click="keyClick">
+      <p>1</p>
+    </button>
+    <button class="two" aria-selected="false" @click="keyClick">
+      <p>2</p>
+    </button>
+    <button class="three" aria-selected="false" @click="keyClick">
+      <p>3</p>
+    </button>
+    <button class="minus" aria-selected="false" @click="keyClick">
+      <p>-</p>
+    </button>
+    <button class="dot" aria-selected="false" @click="keyClick">
+      <p>.</p>
+    </button>
+    <button class="zero" aria-selected="false" @click="keyClick">
+      <p>0</p>
+    </button>
+    <button class="slash" aria-selected="false" @click="keyClick">
+      <p>/</p>
+    </button>
+    <button class="multiply" aria-selected="false" @click="keyClick">
+      <p>x</p>
+    </button>
+    <button class="reset uppercase" aria-selected="false" @click="keyClick">
+      <p>reset</p>
+    </button>
+    <button class="equal" aria-selected="false" @click="keyClick">
+      <p>=</p>
+    </button>
   </section>
 </template>
 
@@ -51,13 +97,31 @@ section {
   border-radius: 8px;
 }
 
-section > div {
+button {
+  appearance: none;
+  border: none;
+  display: block;
+  transition: all 0.05s ease-in;
+}
+
+button:active {
+  transform: translateY(4px);
+  box-shadow: none;
+}
+
+button[aria-selected="true"] {
+  transform: translateY(4px);
+  box-shadow: none;
+}
+
+section > button {
   position: relative;
   @include grid(0);
   font-size: $fs;
+  cursor: pointer;
 }
 
-section > div > p {
+section > button > p {
   position: absolute;
   place-self: center;
   padding-top: 6px;
